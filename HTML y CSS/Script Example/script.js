fd46a3113817c91cc59b98343f11358f4963b5f8
GARCIA DELGADO Fatima <fatima.garcia@soprasteria.com>
2021-03-08 18:13:46 +0100

    Modificacion script

--- HTML y CSS/Script Example/script.js
+++ HTML y CSS/Script Example/script.js
@@ -1,16 +1,8 @@
-/*let objetos = document.getElementsByTagName('body');
-          for (let i = 0; i < objetos.length; i++) {
-         console.log("Elemento: " + objetos[i]);
-                                                }*/
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
     let nodeTree = getNodeTree(document.documentElement);
     //console.log(nodeTree);
-    console.log("TREE NODE FOR MY CV: \n" + showTree(nodeTree, 1));
+    console.log("TREE NODE FOR MY CV: \n" + showTree(nodeTree, 0));
 });
-
 
 
 //console.log(showTree(nodeTree));
@@ -25,40 +17,14 @@ function getNodeTree(node) {
 
         return {
             nodeName: node.nodeName,
-            parentName: node.parentNode.nodeName,
             children: children
         };
     }
     return {
         nodeName: node.nodeName,
-        parentName: node.parentNode.nodeName,
         children: null
     }
 }
-
-/*
-function showTree(node, iteration) {
-    if (!node) return "Error";
-    let txt = "";
-
-    txt += showSpaces(iteration) + "Node: " + node.nodeName + '\n';
-    txt += showSpaces(iteration) + "Parent: " + node.parentName + '\n';
-
-    if (node.children != null) {
-
-        /*txt += "Content: " + node.content + '\n';
-        for (let i = 0; i < node.children.length; ++i) {
-            txt += showSpaces(iteration) + "Children: \n" + showSpaces(iteration) + showTree(node.children[i], iteration++) + "\n";
-            iteration--;
-        }
-
-
-    } else {
-        txt += showSpaces(iteration) + node.nodeName + " has no children\n";
-    }
-
-    return txt;
-}*/
 
 function showTree(node, iteration) {
     if (!node) return "Error";
